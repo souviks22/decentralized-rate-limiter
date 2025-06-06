ec6acd73a91fe5457f542e2fe0bf54a9f3e591d2
Souvik Sarkar <souviksarkar2k3@gmail.com>
2025-06-07 00:16:19 +0530

    CRDT Token Bucket reinforced because CMS is inappropriate

---
+++ internal/limiter/bucket.go
@@ -0,0 +1,54 @@
+package limiter
+
+import (
+	"sync"
+	"time"
+)
+
+type TokenBucket struct {
+	Capacity     float64
+	RefillRate   float64
+	UsedTokens   float64
+	UsableTokens float64
+	LastRefilled time.Time
+	mutex        sync.RWMutex
+}
+
+func newTokenBucket(capacity float64, refillRate float64) *TokenBucket {
+	return &TokenBucket{
+		Capacity:     capacity,
+		RefillRate:   refillRate,
+		UsedTokens:   0,
+		UsableTokens: capacity,
+		LastRefilled: time.Now().UTC(),
+	}
+}
+
+func (tb *TokenBucket) refill() {
+	elapsed := time.Since(tb.LastRefilled).Seconds()
+	tb.LastRefilled = time.Now().UTC()
+	tb.UsableTokens += elapsed * tb.RefillRate
+	tb.UsableTokens = min(tb.UsedTokens+tb.Capacity, tb.UsableTokens)
+}
+
+func (tb *TokenBucket) consume() bool {
+	tb.mutex.Lock()
+	defer tb.mutex.Unlock()
+	tb.refill()
+	if tb.UsableTokens-tb.UsedTokens > 0 {
+		tb.UsedTokens += 1
+		return true
+	}
+	return false
+}
+
+func (tb *TokenBucket) merge(incoming *BucketState) {
+	if incoming == nil {
+		return
+	}
+	tb.mutex.Lock()
+	defer tb.mutex.Unlock()
+	tb.UsedTokens = max(incoming.UsedTokens, tb.UsedTokens)
+	tb.UsableTokens = max(incoming.UsableTokens, tb.UsableTokens)
+	tb.LastRefilled = time.Unix(max(incoming.LastRefilled.Unix(), tb.LastRefilled.Unix()), 0)
+}
